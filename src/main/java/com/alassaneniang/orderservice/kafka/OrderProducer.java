b3f14cd8fe53aad8f01c3593d5ff53d4e104d682
adniang75 <adniang75@gmail.com>
2024-05-12 17:36:03 +0200

    Create Kafka Producer

---
+++ src/main/java/com/alassaneniang/orderservice/kafka/OrderProducer.java
@@ -0,0 +1,31 @@
+package com.alassaneniang.orderservice.kafka;
+
+import com.alassaneniang.basedomains.dto.OrderEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class OrderProducer {
+
+    private final NewTopic topic;
+    private final KafkaTemplate<String, OrderEvent> kafkaTemplate;
+
+    public void sendMessage(OrderEvent event) {
+        log.info("Order event -> {}", event);
+        // create Message
+        Message<OrderEvent> message = MessageBuilder
+                .withPayload(event)
+                .setHeader(KafkaHeaders.TOPIC, topic.name())
+                .build();
+        kafkaTemplate.send(message);
+    }
+
+}
